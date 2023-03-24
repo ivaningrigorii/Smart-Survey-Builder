138a83c839bf79a588ab66b9399aabbe0947bae6
ivaningrigorii <ivanin.grigorii@yandex.ru>
2023-03-24 21:04:19 +0300

    Добавил пользовательские permissions. Протетировал возможность возврата типа создаваемого опроса.

--- src_back/apps/survey_passing/serializers.py
+++ src_back/apps/survey_passing/serializers.py
@@ -3,6 +3,12 @@ from apps.survey_manage.survey_base.models import *
 
 
 class SurveysShowSerializer(serializers.ModelSerializer):
+    type_survey = serializers.SerializerMethodField('get_type_survey')
+
     class Meta:
         model = ISurvey
-        fields = ('pk', 'name', 'time_create')
+        fields = ('pk', 'name', 'time_create', 'type_survey')
+
+    def get_type_survey(self, obj):
+        return type(obj).__name__
+
