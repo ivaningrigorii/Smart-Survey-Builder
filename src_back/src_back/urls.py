5cffaaa609f19b0ed12776ee1d36b76941dd5a99
ivaningrigorii <ivanin.grigorii@yandex.ru>
2023-03-24 20:25:17 +0300

    Обновил urls к приложениям на бэке в django

--- src_back/src_back/urls.py
+++ src_back/src_back/urls.py
@@ -3,5 +3,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/survey-passing/', include('apps.survey_passing.urls'))
+    path('api/v1/passing/', include('apps.survey_passing.urls')),
+    path('api/v1/manage/surv/', include('apps.survey_manage.survey_base.urls')),
+    path('api/v1/manage/quest/', include('apps.survey_manage.question_blocks.urls')),
+    path('api/v1/manage/ans/', include('apps.survey_manage.answer_blocks.urls')),
+    path('api/v1/personal/', include('apps.profile_manage.urls')),
 ]
