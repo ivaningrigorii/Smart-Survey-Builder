eaf5745e8e1efc2437eec008ee6ce7288d1e7744
ivaningrigorii <ivanin.grigorii@yandex.ru>
2023-03-25 03:33:50 +0300

    Добавление api каталогов созданных ранее опросов

--- src_back/apps/profile_manage/urls.py
+++ src_back/apps/profile_manage/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, re_path, include
+from .views import CreatedSurveys
 
 urlpatterns = [
     path('auth/', include('djoser.urls')),
-    path('', include('djoser.urls.jwt')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('cats/created/', CreatedSurveys.as_view()),
+    path('cats/created/<int:pk>/', CreatedSurveys.as_view()),
 ]
--- src_back/apps/survey_passing/views.py
+++ src_back/apps/survey_passing/views.py
@@ -1,8 +1,7 @@
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from apps.survey_manage.survey_base.serializers import SurveysShowSerializer
 
 from apps.survey_manage.survey_base.models import ISurvey
-from apps.survey_passing.serializers import SurveysShowSerializer
 
 
 class SurveyList(generics.ListAPIView):
