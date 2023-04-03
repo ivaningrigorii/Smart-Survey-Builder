90b7957e7443193230b9665a23cd787402b206a1
Kipokape <karandash022@gmail.com>
2023-04-04 01:42:55 +0300

    Тестовая версия api для добавления вопросов

--- src_back/apps/survey_manage/question_blocks/views.py
+++ src_back/apps/survey_manage/question_blocks/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.permissions import IsAuthenticated
+from .serializers import *
+from .models import IQuestion
 
-# Create your views here.
+
+class QuestionSimpleAddAPIView(generics.CreateAPIView):
+    """
+        Создание и добавление нового вопроса в опрос
+    """
+    serializer_class = IQuestionFullSerializer
+    queryset = IQuestion.objects.all()
+    permission_classes = (IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        survey_id = self.request.data.get('survey')
+        if survey_id:
+            survey_owner = serializer.validated_data['survey'].user
+            if survey_owner != self.request.user:
+                raise PermissionDenied()
+        serializer.save()
