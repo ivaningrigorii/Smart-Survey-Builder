a5ba8ec08be2023c3f068830f0f9a07b3a1fa508
Kipokape <karandash022@gmail.com>
2023-04-04 22:56:21 +0300

    Api для передачи Slug

--- src_back/apps/survey_manage/survey_base/urls.py
+++ src_back/apps/survey_manage/survey_base/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, re_path
-from .views import SurveyGetUpdateHeader, SurveyCreateHeaderView
+from .views import *
 
 urlpatterns = [
     path('survey-header/<int:pk>/', SurveyGetUpdateHeader.as_view()),
     path('survey-header/', SurveyCreateHeaderView.as_view()),
+    path('survey-header/<int:pk>/slug/', SurveyGetSlugView.as_view()),
 ]
+
+
