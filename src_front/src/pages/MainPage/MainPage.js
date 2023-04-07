8b5504dca6e799ef81d06d037f41b344747bda79
IrinaCol28 <irina.col2626@gmail.com>
2023-04-07 16:19:16 +0300

    b

--- src_front/src/pages/MainPage/MainPage.js
+++ src_front/src/pages/MainPage/MainPage.js
@@ -2,7 +2,6 @@ import Header from "../../components/Header/Header";
 import  React, { Component } from  'react';
 import Footer from "../../components/Footer/Footer";
 import MainPageInf from "./components/MainPageInf/MainPageInf";
-
 class  MainPage  extends  Component {
 
     constructor(props) {
