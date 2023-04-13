ff7352b75c1da0ead9ce25d61429b440a6df4a78
IrinaCol28 <irina.col2626@gmail.com>
2023-04-13 20:48:23 +0300

    тип опроса

--- src_front/src/pages/CreatePage/components/ChangeTypeOfPoll.js
+++ src_front/src/pages/CreatePage/components/ChangeTypeOfPoll.js
@@ -1,74 +1,8 @@
-
-import { Select, MenuItem} from '@mui/material';
-import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
-import { makeStyles } from '@material-ui/core/styles';
 import StartTime from './StartTime';
 import EndTime from './EndTime';
 import TimePassing from './TimePassing';
 import Box from '@mui/material/Box';
-
-// export default function ChangeTypeOfPoll() {
-//   const [resourcetype, setResourcetype] = useState('SurveySimple');
-//   const [showTimeFields, setShowTimeFields] = useState(false);
-
-//   const handleSelectChange = (selectedOption) => {
-//     setResourcetype(selectedOption);
-//     setShowTimeFields(selectedOption=== "SurveyTest");
-//     console.log(`Option selected:`, selectedOption);
-//   };
- 
-//   // const handleSelectChange = resourcetype => {
-//   //   setResourcetype({ resourcetype });
-//   //   setShowTimeFields(resourcetype==="SurveyTest");
-//   //   console.log(`Option selected:`, resourcetype.target.value);
-//   // };
-
-
-//   const useStyles = makeStyles((theme) => ({
-//     root: {
-//       display: 'flex',
-//       flexWrap: 'wrap',
-//     },
-//     Select: {
-//       marginLeft: theme.spacing(1),
-//       marginRight: theme.spacing(1),
-//       width: '25ch',
-//     },
-//   }));
-
-//   return (
-//     <div>
-//         <Box  sx={{ marginTop:2, flexGrow: 1,  width: '95%',
-//         maxWidth: '100%'}}>         
-//          <FormControl sx={{ m: 1, minWidth: 120 }}>
-//       <InputLabel id="demo-simple-select-helper-label">Тип опроса</InputLabel>
-//       <Select
-//        labelId="demo-simple-select-helper-label"
-//         value={resourcetype}
-//         id="resourcetype"
-//         onChange={handleSelectChange}
-//         label="Тип опроса"
-//         helperText="Выберите тип опроса"
-//         sx={{ m: 1, minWidth: 130 }}
-//         inputProps={{ ref: (input) => input && input.focus() }}
-//       >
-//         <MenuItem value="SurveySimple">Простой</MenuItem>
-//         <MenuItem value="SurveyTest">Тестовый</MenuItem>
-//       </Select>
-//       {showTimeFields && (
-//         <div>
-//           <StartTime/>
-//           <EndTime/>
-//           <TimePassing/>
-//         </div>
-//       )}
-//       </FormControl>
-//       </Box>    
-//     </div>
-//   );
-// }
-
 import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
