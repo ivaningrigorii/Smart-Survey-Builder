de4ce3a61c2bb6bc454f6fb55ca6e563225da2bc
IrinaCol28 <irina.col2626@gmail.com>
2023-04-13 20:45:50 +0300

    Исправлен выбор типа опроса

--- src_front/src/pages/CreatePage/components/ChangeTypeOfPoll.js
+++ src_front/src/pages/CreatePage/components/ChangeTypeOfPoll.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+
 import { Select, MenuItem} from '@mui/material';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
@@ -8,48 +8,102 @@ import EndTime from './EndTime';
 import TimePassing from './TimePassing';
 import Box from '@mui/material/Box';
 
-export default function ChangeTypeOfPoll() {
-  const [resourcetype, setResourcetype] = useState('');
+// export default function ChangeTypeOfPoll() {
+//   const [resourcetype, setResourcetype] = useState('SurveySimple');
+//   const [showTimeFields, setShowTimeFields] = useState(false);
+
+//   const handleSelectChange = (selectedOption) => {
+//     setResourcetype(selectedOption);
+//     setShowTimeFields(selectedOption=== "SurveyTest");
+//     console.log(`Option selected:`, selectedOption);
+//   };
+ 
+//   // const handleSelectChange = resourcetype => {
+//   //   setResourcetype({ resourcetype });
+//   //   setShowTimeFields(resourcetype==="SurveyTest");
+//   //   console.log(`Option selected:`, resourcetype.target.value);
+//   // };
+
+
+//   const useStyles = makeStyles((theme) => ({
+//     root: {
+//       display: 'flex',
+//       flexWrap: 'wrap',
+//     },
+//     Select: {
+//       marginLeft: theme.spacing(1),
+//       marginRight: theme.spacing(1),
+//       width: '25ch',
+//     },
+//   }));
+
+//   return (
+//     <div>
+//         <Box  sx={{ marginTop:2, flexGrow: 1,  width: '95%',
+//         maxWidth: '100%'}}>         
+//          <FormControl sx={{ m: 1, minWidth: 120 }}>
+//       <InputLabel id="demo-simple-select-helper-label">Тип опроса</InputLabel>
+//       <Select
+//        labelId="demo-simple-select-helper-label"
+//         value={resourcetype}
+//         id="resourcetype"
+//         onChange={handleSelectChange}
+//         label="Тип опроса"
+//         helperText="Выберите тип опроса"
+//         sx={{ m: 1, minWidth: 130 }}
+//         inputProps={{ ref: (input) => input && input.focus() }}
+//       >
+//         <MenuItem value="SurveySimple">Простой</MenuItem>
+//         <MenuItem value="SurveyTest">Тестовый</MenuItem>
+//       </Select>
+//       {showTimeFields && (
+//         <div>
+//           <StartTime/>
+//           <EndTime/>
+//           <TimePassing/>
+//         </div>
+//       )}
+//       </FormControl>
+//       </Box>    
+//     </div>
+//   );
+// }
+
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import { useState } from 'react';
+
+const options = ['Обычный опрос', 'Тестовый опрос'];
+
+export default function ControllableStates() {
+  const [resourcetype, setValue] = React.useState(options[0]);
+  const [inputValue, setInputValue] = React.useState('');
   const [showTimeFields, setShowTimeFields] = useState(false);
-
-  const handleSelectChange = (event) => {
-    setResourcetype(event.target.value);
-    setShowTimeFields(event.target.value === 'SurveyTest');
-  };
-
-
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-      flexWrap: 'wrap',
-    },
-    Select: {
-      marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(1),
-      width: '25ch',
-    },
-  }));
 
   return (
     <div>
-        <Box  sx={{ marginTop:2, flexGrow: 1,  width: '95%',
+         <Box  sx={{ marginTop:2, flexGrow: 1,  width: '95%',
         maxWidth: '100%'}}>         
          <FormControl sx={{ m: 1, minWidth: 120 }}>
-      <InputLabel id="demo-simple-select-helper-label">Тип опроса</InputLabel>
-      <Select
-       labelId="demo-simple-select-helper-label"
+      <Autocomplete
         value={resourcetype}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        inputValue={inputValue}
+        onInputChange={(event, newInputValue) => {
+          
+          setInputValue(newInputValue);
+          setShowTimeFields(newInputValue=== "Тестовый опрос");
+        }}
         id="resourcetype"
-        onChange={handleSelectChange}
-        label="Тип опроса"
-        helperText="Выберите тип опроса"
-        sx={{ m: 1, minWidth: 130 }}
-        inputProps={{ ref: (input) => input && input.focus() }}
-      >
-        <MenuItem value="SurveySimple" >Простой</MenuItem>
-        <MenuItem value="SurveyTest">Тестовый</MenuItem>
-      </Select>
-      {showTimeFields && (
+        options={options}
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params}  label="Тип вопроса" /> }
+
+      />
+         {showTimeFields && (
         <div>
           <StartTime/>
           <EndTime/>
@@ -57,7 +111,7 @@ export default function ChangeTypeOfPoll() {
         </div>
       )}
       </FormControl>
-      </Box>    
+            </Box>  
     </div>
   );
 }
--- src_front/src/pages/CreatePage/components/CreatePoll.js
+++ src_front/src/pages/CreatePage/components/CreatePoll.js
@@ -7,16 +7,29 @@ import plus_url_server_api_v1 from './plus_url_server';
 
 function CreatePoll() {
 
-  const handleSubmit = (event) => {
+  const DeterminePollTypeFromAutocomplete = function(res) {
+   if(res==='Обычный опрос')
+    return "SurveySimple";
+    if(res==='Тестовый опрос')
+    return "SurveyTest";
+  }
+  const handleSubmit = (event, selectedOption) => {
     event.preventDefault();
+
     const data = {
       name: event.target.name.value,
       description: event.target.description.value,
-      // resourcetype: event.target.resourcetype.value,
+      resourcetype: DeterminePollTypeFromAutocomplete(event.target.resourcetype.value),
       start_time: event.target.start_time.value,
-      end_time: event.target.end_time.value,
+     end_time: event.target.end_time.value,
       time_passing: event.target.time_passing.value
     };
+    console.log(event.target.name.value);
+    console.log(event.target.description.value);
+    console.log(DeterminePollTypeFromAutocomplete(event.target.resourcetype.value));
+    console.log(event.target.start_time.value);
+    console.log(event.target.end_time.value);
+    console.log(event.target.time_passing.value);
   
     fetch(plus_url_server_api_v1("/manage/surv/survey-header/"), {
       method: 'POST',
