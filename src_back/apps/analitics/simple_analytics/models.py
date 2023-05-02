1d70ef2f9c10cef702c33407178b0d969757e14f
Kipokape <karandash022@gmail.com>
2023-05-03 02:04:46 +0300

    Модель SimpleAnalytics для простой аналитики

--- src_back/apps/analitics/simple_analytics/models.py
+++ src_back/apps/analitics/simple_analytics/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from apps.survey_manage.question_blocks.models import IQuestion
+from apps.survey_manage.survey_base.models import ISurvey
+
+
+class SimpleAnalytics(models.Model):
+    """ Простая аналитика (кол-во выборов ответа) """
+    survey = models.ForeignKey(ISurvey, on_delete=models.CASCADE, verbose_name='Опрос')
+    question = models.ForeignKey(IQuestion, on_delete=models.CASCADE, verbose_name='Вопрос')
+
+    class Meta:
+        verbose_name = 'простая аналитика (SimpleAnalytics)'
+        verbose_name_plural = 'вся простая аналитика (SimpleAnalytics)'
+        db_table = 'SimpleAnalytics'
+
