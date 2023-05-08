aed03f8b0626070a6f496822a1f30b0a50dd756d
Дмитрий <bityukov.com@gmail.com>
2023-05-08 21:41:29 +0300

    Заливаю только функции из PassingServices.js

--- src_front/src/pages/polls_/passing/PassingServices.js
+++ src_front/src/pages/polls_/passing/PassingServices.js
@@ -35,5 +35,52 @@ class PassingServices {
             });
     }
 
+     PassingOprosa(id) {
+       try {
+            if (!id || id <= 0) {
+                throw new Error();
+            }
+           return axios.get(reverse("\n" +
+               "api/v1/list_questions/:id/", {id:id}), {
+               headers: {
+                    'Authorization': 'Bearer ' + _token(),
+                }
+            })
+                .then((response) => {
+                    return Promise.resolve(response.data);
+                })
+                .catch(function (error) {
+                    return Promise.reject(error);
+                });
+        } catch (error) {
+            return Promise.reject(error);
+        }
+    }
+
+    //Сохранение или изменение сведений об ответах
+    SaveAnswer(id, result_questions) {
+        return axios.post(reverse("\n" +
+            "api/v1/list_questions/:id/", {id: id}), {
+            result_questions: result_questions,
+            headers: {
+                'Authorization': 'Bearer ' + _token(),
+            }
+        })
+            .then((resp) => {
+                return Promise.resolve(resp);
+            })
+            .catch((err) => {
+                return Promise.reject(err);
+            })
+    }
+    PassingEnd(id) {
+       let path = reverse("/passing/taking_survey/end/:id/", {id: id});
+       return axios.patch(path, id, {
+           headers: { 'Authorization': 'Bearer ' + _token(), }
+       })
+      .then((respone)=>{ return Promise.resolve(respone.data);})
+      .catch((error)=>{ return Promise.reject(error);})
+    }
+
 }
 export default PassingServices;
