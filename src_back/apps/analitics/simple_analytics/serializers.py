45200b8b5ec6dbb15fad7bd7fd1089ac54838d6e
Kipokape <karandash022@gmail.com>
2023-05-09 19:33:20 +0300

    Добавление вывода вариантов ввода и их кол-ва

--- src_back/apps/analitics/simple_analytics/serializers.py
+++ src_back/apps/analitics/simple_analytics/serializers.py
@@ -1,10 +1,11 @@
+from django.db.models import Count
 from rest_framework import serializers
 
 from apps.analitics.simple_analytics.models import SimpleAnalytics
-from apps.survey_manage.answer_blocks.models import IAnswer
+from apps.survey_manage.answer_blocks.models import IAnswer, AnswerTextInput
 from apps.survey_manage.answer_blocks.serializers import IAnswerFullSerializer
 from apps.survey_manage.survey_base.models import ISurvey
-from apps.survey_passing.models import IResultAnswer, TakingSurvey
+from apps.survey_passing.models import IResultAnswer, TakingSurvey, ResultTextInput
 
 
 class SimpleAnalyticsSerializer(serializers.ModelSerializer):
@@ -25,8 +26,19 @@ class SimpleAnalyticsSerializer(serializers.ModelSerializer):
         )
 
         for answer in data['answers']:
+            if answer['resourcetype'] == 'AnswerTextInput':
+                result_text_input_answers = {}
+                result_text_input_objects = result_answer.instance_of(ResultTextInput)
+                for result_text_input in result_text_input_objects:
+                    text_input_answer = result_text_input.input_text.lower()
+                    result_text_input_answers[text_input_answer] = result_text_input_answers.get(text_input_answer,
+                                                                                                 0) + 1
+
+                data["result_text_input_answers"] = result_text_input_answers
+
             answer_count = result_answer.filter(answer=answer['id']).count()
             answer['count'] = answer_count
+
         return data
 
 
@@ -40,4 +52,3 @@ class NumberPassingSerializer(serializers.ModelSerializer):
     def get_number_passing(self, obj):
         passing_count = TakingSurvey.objects.filter(survey=obj.id, is_completed=True).count()
         return passing_count
-
