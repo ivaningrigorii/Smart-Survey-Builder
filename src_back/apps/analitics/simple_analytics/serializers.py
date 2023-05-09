ccb27d691d9d6646d319801f78413e2fb390f600
Kipokape <karandash022@gmail.com>
2023-05-09 18:26:20 +0300

    Api для получения общего числа прохождений опроса

--- src_back/apps/analitics/simple_analytics/serializers.py
+++ src_back/apps/analitics/simple_analytics/serializers.py
@@ -3,7 +3,8 @@ from rest_framework import serializers
 from apps.analitics.simple_analytics.models import SimpleAnalytics
 from apps.survey_manage.answer_blocks.models import IAnswer
 from apps.survey_manage.answer_blocks.serializers import IAnswerFullSerializer
-from apps.survey_passing.models import IResultAnswer
+from apps.survey_manage.survey_base.models import ISurvey
+from apps.survey_passing.models import IResultAnswer, TakingSurvey
 
 
 class SimpleAnalyticsSerializer(serializers.ModelSerializer):
@@ -27,4 +28,16 @@ class SimpleAnalyticsSerializer(serializers.ModelSerializer):
             answer_count = result_answer.filter(answer=answer['id']).count()
             answer['count'] = answer_count
         return data
+
+
+class NumberPassingSerializer(serializers.ModelSerializer):
+    number_passing = serializers.SerializerMethodField()
+
+    class Meta:
+        model = ISurvey
+        fields = ["id", "number_passing"]
+
+    def get_number_passing(self, obj):
+        passing_count = TakingSurvey.objects.filter(survey=obj.id, is_completed=True).count()
+        return passing_count
 
